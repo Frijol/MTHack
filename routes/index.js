5fbbd183c834f42434678dc35db35a5a65d56364
Frijol <ifoundthemeaningoflife@gmail.com>
2014-03-29 01:37:51 -0400

    now with basic webapp framework

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
+};
+
+exports.process = function(req, res){
+  console.log(req);
+  res.redirect('/')
 };
